c62bb66b53e4bcdea48910c8c4c006f2c28e6f9b
Dmitry-Romanchuk-Unilimes <dmitry.romanchuk@unilimes.coms>
2022-01-05 10:30:21 +0200

    finished lesson 12

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-import { useState, useRef } from 'react';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { Canvas, useThree, extend, useFrame } from 'react-three-fiber'
+import React, { useState, useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Canvas, useThree, extend, useFrame } from "react-three-fiber";
 import { useSpring } from 'react-spring'
 import { a } from '@react-spring/three'
+
+import "./App.css";
 
 extend({ OrbitControls });
 
@@ -12,60 +13,79 @@ function Cube(props) {
   const [isHovered, setIsHovered] = useState(false);
   const ref = useRef();
 
-  const { size, x } = useSpring({
-    size: isBig ? [2, 2, 2] : [1, 1, 1],
-    x: isBig ? 2 : 0
-  });
-
-  const color = isHovered ? 'pink' : 'salmon';
-
   useFrame(() => {
     ref.current.rotation.x += 0.01;
     ref.current.rotation.y += 0.01;
   });
 
+  const { size, x } = useSpring({
+    size: isBig ? [2, 2, 2] : [1, 1, 1],
+    x: isBig ? 2 : 0,
+  });
+
+  const color = isHovered ? "pink" : "salmon";
+
   return (
-    <a.mesh {...props}
+    <a.mesh
+      {...props}
       ref={ref}
       scale={size}
+      position-x={x}
+      castShadow={true}
+      receiveShadow={true}
       onClick={() => setIsBig(!isBig)}
-      onPointerOver={() => setIsHovered(true)}
       onPointerOut={() => setIsHovered(false)}
-      position-x={x}>
-      <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
-      {/* <sphereBufferGeometry attach='geometry' args={[1, 8, 6]} /> */}
-      <meshPhysicalMaterial attach='material' color={color} roughness={1} metalness={0.5} clearcoat={1} />
+      onPointerOver={() => setIsHovered(true)}
+    >
+      <sphereBufferGeometry attach="geometry" args={[1, 8, 6]} />
+      <meshPhongMaterial
+        flatShading={true}
+        roughness={1}
+        metalness={0.5}
+        shininess={100}
+        attach="material"
+        color={color}
+      />
     </a.mesh>
-  )
+  );
 }
+// box args = [width, height, depth ]
+// sphere args = [width, height, depth ]
 
 function Plane() {
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]}>
-      <planeBufferGeometry attach='geometry' args={[10, 10]} />
-      <meshStandardMaterial attach='material' color='#d3d3d3' />
+    <mesh
+      receiveShadow={true}
+      rotation={[-Math.PI / 2, 0, 0]}
+      position={[0, -2, -5]}
+    >
+      <planeBufferGeometry attach="geometry" args={[20, 20]} />
+      <meshPhongMaterial attach="material" color="#D3D3D3" />
     </mesh>
-  )
+  );
 }
 
 function Scene() {
-  const { camera, gl: { domElement } } = useThree();
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
 
   return (
     <>
       <ambientLight />
-      <pointLight position={[-1, 2, 4]} intensity={0.3} />
-      <Cube rotation={[10, 10, 10]} position={[0, 0, 0]} />
-      <Cube rotation={[10, 20, 10]} position={[2, 2, 0]} />
+      <spotLight castShadow={true} intensity={0.6} position={[0, 10, 4]} />
+      <Cube rotation={[10, 10, 0]} position={[0, 0, 0]} />
+      <Cube rotation={[10, 20, 0]} position={[2, 2, 0]} />
       <Plane />
       <orbitControls args={[camera, domElement]} />
     </>
-  )
+  );
 }
 
 function App() {
   return (
-    <Canvas>
+    <Canvas shadowMap={true}>
       <Scene />
     </Canvas>
   );
